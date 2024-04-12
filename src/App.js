b0ec2926ed666c593c439b4eb139141f1d7d7595
enoquezhou1 <166862098+enoquezhou1@users.noreply.github.com>
2024-04-12 16:25:11 -0300

    feat(p1-pdmt): cria componente principal e faz hello

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+
+function App() {
+  return (
+<div class="container">
+  <div class="row">
+    <div class="w-100"></div>
+    <div class="col">Hello, tarefas</div>
+  </div>
+</div>
+  );
+}
+
+export default App;
